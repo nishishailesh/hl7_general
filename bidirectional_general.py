51cdd9f06a438b8a73ea860c985f9b12c51ede8d
Your Name <you@example.com>
2022-08-26 01:15:01 +0530

    improved client message of ACK send by server

--- bidirectional_general.py
+++ bidirectional_general.py
@@ -2,8 +2,7 @@
 import sys,time, logging, socket, select, os
 import bidirectional_general_conf as conf
 
-def print_to_log(object1,object2):
-  logging.debug('{} {}'.format(object1,object2))
+
   
 class bdg(object):
   read_msg=b''
@@ -17,8 +16,22 @@ class bdg(object):
   writable=set()
   exceptional=set()
 
+
+
+  def print_to_log(self,object1,object2,loglevel=logging.DEBUG):
+    if(loglevel==logging.INFO):
+      logging.info('{} {} {}'.format(logging.INFO,object1,object2))
+    elif(loglevel==logging.WARNING):
+      logging.warning('{} {}'.format(object1,object2))  
+    elif(loglevel==logging.ERROR):
+      logging.error('{} {}'.format(object1,object2))
+    elif(loglevel==logging.CRITICAL):
+      logging.critical('{} {}'.format(object1,object2))    
+    else:
+      logging.debug('{} {} {}'.format(logging.DEBUG,object1,object2))
+
   def list_wait(self):
-    print_to_log('Listening to {} , {} , {} '.
+    self.print_to_log('Listening to {} , {} , {} '.
                     format(
                             list(map(socket.socket.fileno,self.read_set)),
                             list(map(socket.socket.fileno,self.write_set)),
@@ -37,21 +50,21 @@ class bdg(object):
   #override this function in subclass, it donot read data, just manages it after getting data from loop
   ###################################
   def manage_read(self,data):
-    print_to_log("bdg::loop()->bdg::manage_read():",data)
+    self.print_to_log("bdg::loop()->bdg::manage_read():",data)
     self.write_msg=b"bdg::manage_read():"+data
   
   ###################################
   #override this function in subclass, it actually sends data
   ###################################
   def manage_write(self):      
-    print_to_log('bdg::loop->bdg::manage_write():','')       
+    self.print_to_log('bdg::loop->bdg::manage_write():','')       
     if(len(self.write_msg)>0):
-      print_to_log('bdg::manage_write():Following will be sent',self.write_msg) 
+      self.print_to_log('bdg::manage_write():Following will be sent',self.write_msg) 
       try:
         self.conn[0].send(self.write_msg)
         self.write_msg=''  
       except Exception as my_ex :
-        print_to_log("Disconnection from client?",my_ex)                    
+        self.print_to_log("Disconnection from client?",my_ex)                    
          
   def __init__(self,host_address,host_port,select_timeout):
     #logging.basicConfig(filename=conf.log_filename,level=logging.CRITICAL)
@@ -69,22 +82,22 @@ class bdg(object):
     try:
       self.s.bind((self.host_address,int(self.host_port)))	#it is a tuple
     except Exception as my_exception:      
-      print_to_log(my_exception,'bind() failed, ip/port correct??Quitting')
+      self.print_to_log(my_exception,'bind() failed, ip/port correct??Quitting')
       quit()      
     self.s.listen(2)
 
-    print_to_log(self.s,'select() is waiting..') 
+    self.print_to_log(self.s,'select() is waiting..') 
     self.readable, self.writable, self.exceptional = select.select((self.s,),(self.s,),(self.s,))
-    print_to_log(self.s,'select() detected activity')
+    self.print_to_log(self.s,'select() detected activity')
     if(self.s in self.exceptional):
-      print_to_log(self.s,'some error on socket s. quitting')
+      self.print_to_log(self.s,'some error on socket s. quitting')
       quit() 
     if(self.s in self.writable):
-      print_to_log(self.s,'Can not understand why s is writting')
+      self.print_to_log(self.s,'Can not understand why s is writting')
       quit() 
     if(self.s in self.readable):
       self.conn = self.s.accept()
-      print_to_log(self.s,'Connection request is read')
+      self.print_to_log(self.s,'Connection request is read')
 
       #Housekeeping read_set etc. after new connection
       self.conn[0].setblocking(0)    
@@ -100,50 +113,50 @@ class bdg(object):
     #not tuple it is unmutable
     #not list, we need uniq values in error list which is sum of read and write
     while True:      
-      #self.print_to_log('','before select')
+      #self.self.print_to_log('','before select')
       self.readable, self.writable, self.exceptional = select.select(self.read_set,self.write_set,self.error_set,self.select_timeout)
-      #self.print_to_log('','after select')
+      #self.self.print_to_log('','after select')
       self.list_wait()
       ###if anybody else try to connect, reject it
 
       if(self.s in self.exceptional):
-        print_to_log(self.s,'some error on socket s. quitting')
+        self.print_to_log(self.s,'some error on socket s. quitting')
         self.s.shutdown(socket.SHUT_RDWR)
         self.s.close()
         break 
 
       if(self.s in self.writable):
-        print_to_log(self.s,'Can not understand why s is writting')
+        self.print_to_log(self.s,'Can not understand why s is writting')
         self.s.shutdown(socket.SHUT_RDWR)
         self.s.close()
         break 
 
       if(self.s in self.readable):
         dummy_conn = self.s.accept()
-        print_to_log(self.s,'Connection request is read, This is second connection. We do not want it. shutdown, close')
+        self.print_to_log(self.s,'Connection request is read, This is second connection. We do not want it. shutdown, close')
         dummy_conn[0].shutdown(socket.SHUT_RDWR)
         dummy_conn[0].close()
         
       ###For client do work
       if(self.conn[0] in self.exceptional):
-        print_to_log(self.conn[0],'some error on socket conn. quitting')
+        self.print_to_log(self.conn[0],'some error on socket conn. quitting')
         self.s.shutdown(socket.SHUT_RDWR)
         self.s.close()
         break 
 
       if(self.conn[0] in self.writable):
         #sending message (somewhere else conn[0] was added in writable and self.write_msg was given value
-        print_to_log(self.conn[0],'conn is writable. using manage_write()')
+        self.print_to_log(self.conn[0],'conn is writable. using manage_write()')
         self.manage_write() 
                        
       if(self.conn[0] in self.readable):
-        print_to_log(self.conn[0],'Conn have sent some data. now using recv() and manage_read()')
+        self.print_to_log(self.conn[0],'Conn have sent some data. now using recv() and manage_read()')
         try:
           data=self.conn[0].recv(1024)
-          print_to_log('Following is received:',data)  
+          self.print_to_log('Following is received:',data)  
           self.manage_read(data)
         except Exception as my_exception:      
-          print_to_log(my_exception,'recv() failed. something sent and then connection closed') 
+          self.print_to_log(my_exception,'recv() failed. something sent and then connection closed') 
           self.s.shutdown(socket.SHUT_RDWR)
           self.s.close()
           '''to prevent: DEBUG:root:[Errno 110] Connection timed out recv() failed. something sent and then connection closed
@@ -153,14 +166,14 @@ class bdg(object):
         #only EOF is handled here, rest is handled in manage_read()
         #if EOF 1)close socket 2)remove from list 3)accept new
         if(data==b''):
-          print_to_log(self.conn[0],'Conn have closed, accepting new connection')
+          self.print_to_log(self.conn[0],'Conn have closed, accepting new connection')
           
           #1) close socket
           try:
             self.conn[0].shutdown(socket.SHUT_RDWR)
             self.conn[0].close()
           except Exception as my_ex:
-            print_to_log('Connection from client closed??',my_ex)  
+            self.print_to_log('Connection from client closed??',my_ex)  
 			  
           #2)remove from read list
           
@@ -176,7 +189,7 @@ class bdg(object):
           
           #3) Accept new, add to read set, this is blocking here. No need to go for initiate_write, because nothing to do
           self.conn = self.s.accept()
-          print_to_log(self.s,'New Connection request is read')  
+          self.print_to_log(self.s,'New Connection request is read')  
           
           #Housekeeping read_set etc. after new connection
           self.conn[0].setblocking(0)    
--- hl7_general.py
+++ hl7_general.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 import sys,time, logging, socket, select, os, signal
 import bidirectional_general_conf as conf
-from bidirectional_general import bdg, print_to_log
+from bidirectional_general import bdg
 from file_management_lis import file_management_lis
 from mysql_lis import mysql_lis
 
-def print_to_log(object1,object2):
-  logging.debug('{} {}'.format(object1,object2))
-  
+    
 class hl7(bdg):
   
   def __init__(self,host_address,host_port,select_timeout,alarm_time):
@@ -19,16 +17,15 @@ class hl7(bdg):
     self.mllp_start_byte=b'\x0b'
     self.mllp_end_byte=b'\x1c'
     self.mllp_newline=b'\x0d'
-    
+ 
+    #Possible message status
     #START_BYTE_RECEIVED
-    #RECEIVING_CONTENT
-    #RECEIVED_NEWLINE
     #END_BYTE_RECEIVED
     
     signal.signal(signal.SIGALRM, self.signal_handler)
 
   def manage_read(self,data):
-    print_to_log("hl7::manage_read():",data)
+    self.print_to_log("hl7::manage_read():",data)
     self.analyse_read_data(data)
     '''
     if(self.is_received_data_MLLP(data)):
@@ -37,75 +34,102 @@ class hl7(bdg):
       self.write_msg=b'Error: >>>>Received data is NOT MLLP'
     '''
     
-    print_to_log('hl7::manage_read(): sending...',self.write_msg)       
+    self.print_to_log('hl7::manage_read(): sending...',self.write_msg)       
 
   def manage_write(self):
-    print_to_log('hl7::manage_write():','I am called by loop()')       
+    self.print_to_log('hl7::manage_write():','I am called by loop()')       
     #Send message in response to write_set->select->writable initiated by manage_read() and initiate_write()
     if(len(self.write_msg)>0):
-      print_to_log('hl7::manage_write():Following will be sent',self.write_msg) 
+      self.print_to_log('hl7::manage_write():Following will be sent',self.write_msg) 
       try:
         self.conn[0].send(self.write_msg)
         self.write_msg='' #not in astm. because status 
       except Exception as my_ex :
-        print_to_log("Disconnection from client?",my_ex)                    
+        self.print_to_log("Disconnection from client?",my_ex)                    
 
   def signal_handler(self,signal, frame):
-    print_to_log('Alarm Stopped','Signal:{} Frame:{}'.format(signal,frame))
-    print_to_log('Alarm..response NOT received in stipulated time','data receving/sending may be incomplate')
+    self.print_to_log('Alarm Stopped','Signal:{} Frame:{}'.format(signal,frame))
+    self.print_to_log('Alarm..response NOT received in stipulated time','data receving/sending may be incomplate')
 
 
   def analyse_read_data(self,data):
-    if(self.hl7_message_status=='EMPTY'):
-      for i in data:
-        if(i==self.mllp_start_byte):
-          self.hl7_message_status='START_BYTE_RECEIVED'
-          self.hl7_message=()
+    self.print_to_log("hl7::analyse_read_data():",data)
+    for d in data:
+      i=chr(d).encode()
+      self.print_to_log(self.hl7_message_status,i)
+      if(i==self.mllp_start_byte):
+        self.hl7_message_status='START_BYTE_RECEIVED'
+        self.hl7_message=()
+        self.hl7_line=b''
+        continue
+      if(self.hl7_message_status=='START_BYTE_RECEIVED'):
+        if(i==self.mllp_newline):
+          self.hl7_message=self.hl7_message+(self.hl7_line,)
           self.hl7_line=b''
-          continue
-        if(self.hl7_message_status=='START_BYTE_RECEIVED'):
-          if(i==self.mllp_newline):
-            self.hl7_message=self.hl7_message+(self.hl7_line,)
-            self.hl7_line=b''
-            continue
-          else:
-            self.hl7_line=self.hl7_line+chr(i).encode()
           continue
         if(i==self.mllp_end_byte):
-          elf.hl7_message_status='END_BYTE_RECEIVED'
-          self.hl7_message=self.hl7_message+(self.hl7_line)
+          self.hl7_message_status='END_BYTE_RECEIVED'
+          self.hl7_message=self.hl7_message+(self.hl7_line,)
           self.hl7_line=b''
           continue
-        if(self.hl7_message_status=='END_BYTE_RECEIVED'):
-          if(i==self.mllp_newline):
-            self.process_hl7_message(self.hl7_message)
-            continue
-          else:
-            print_to_log("END_BYTE_RECEIVED, but,","MLLP NEW LINE not received immediately after. Invalid message??")
-            print_to_log("So, Initializing hl7_line,","Initializing hl7_message")
-            self.hl7_message=()
-            self.hl7_line=b''
-            self.hl7_message_status='EMPTY'            
+        else:
+          self.hl7_line=self.hl7_line+i
+        continue          
+      if(self.hl7_message_status=='END_BYTE_RECEIVED'):
+        if(i==self.mllp_newline):
+          self.process_hl7_message(self.hl7_message)
+          self.print_to_log("self.message_as_tuple:",self.message_as_tuple,logging.INFO)
+         
+          self.hl7_message=()
+          self.hl7_line=b''
+          self.hl7_message_status='EMPTY'
+          continue
+        else:
+          self.print_to_log("END_BYTE_RECEIVED, but,","MLLP NEW LINE not received immediately after. Invalid message??")
+          self.print_to_log("So, Initializing hl7_line,","Initializing hl7_message")
+          self.hl7_message=()
+          self.hl7_line=b''
+          self.hl7_message_status='EMPTY'
           continue          
+
+  def process_hl7_message(self,message):
+    self.print_to_log("========Message========\n",message,logging.INFO)
+    self.message_as_tuple=()
+    for i in message:
+      self.print_to_log("Message lines:",i,logging.INFO)
+      line_list=self.process_hl7_line(i)
+      self.message_as_tuple=self.message_as_tuple+(line_list,)
+    self.analyse_message_tuple()
+    self.write_msg=b"\x0bMSH|^~\&|ADT1|MCM|This is ACK message|MCM|198808181126|SECURITY|ADT^A01|"+self.message_as_tuple[0][9]+b"|P|2.3.1\x0dMSA|AA\x1c\x0d"
+
+  def process_hl7_line(self,line):
+    line_list=line.split(b"|")
+    self.print_to_log("line_list:",line_list,logging.INFO)
+    return line_list
+
+  def analyse_message_tuple(self):
+    if(self.message_as_tuple[0][0]==b'MSH'):
+      self.print_to_log("MSH is first field of first line:",self.message_as_tuple[0][0])
+    else:
+      self.print_to_log("MSH is not first field of first line:",self.message_as_tuple[0][0])
+
+    
+  #not yet used in current script
   def is_received_data_MLLP(self,data):
     #MESSAGE=b"\x0bMSH|^~\&|ADT1|MCM|FINGER|MCM|198808181126|SECURITY|ADT^A01|MSG00001|P|2.3.1\x0dPID|1||PATID1234^5^M11^ADT1^MR^MCM~123456789^^^USSSA^SS||\x1c\x0d"
     start_byte=data[0:1]
     end_bytes=data[len(data)-2:]
-    print_to_log(b"required start byte is \x0b , data start bye is:",start_byte)
-    print_to_log(b"required two end bytes are \x1c\x0d , data end two byes are is:",end_bytes)
+    self.print_to_log(b"required start byte is \x0b , data start bye is:",start_byte)
+    self.print_to_log(b"required two end bytes are \x1c\x0d , data end two byes are is:",end_bytes)
     if((start_byte,end_bytes)==(b'\x0b',b'\x1c\x0d')):
-      print_to_log((start_byte,end_bytes) , (start_byte,end_bytes))
+      self.print_to_log((start_byte,end_bytes) , (start_byte,end_bytes))
       return True
     return False
-  def process_hl7_message(self,message):
-    print_to_log("========Message========\n",message)
     
-
-
-
     
 if __name__=='__main__':
-  logging.basicConfig(filename=conf.log_filename,level=logging.DEBUG,format='%(asctime)s : %(message)s') 
+  loglevel=logging.DEBUG
+  logging.basicConfig(filename=conf.log_filename,level=loglevel,format='%(asctime)s : %(message)s') 
 
   print('__name__ is ',__name__,',so running code')
   while True:
@@ -130,5 +154,4 @@ For MLLP (Minimal Lower Layer Protocol):
       ETX or EOT.
   = Carriage Return (1 byte)
       The ASCII carriage return character, i.e., <0x0D>.
-
 '''
